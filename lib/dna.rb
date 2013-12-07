376fc30bf87771ce62827570b349be91e70dacfd
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2013-12-07 16:12:59 +0000

    Add fitness method

--- lib/dna.rb
+++ lib/dna.rb
@@ -1,3 +1,5 @@
+require 'mathn'
+
 class Dna
   attr_reader :phrase
 
@@ -6,6 +8,17 @@ class Dna
 
   def initialize
     @phrase = @@TARGET_PHRASE.length.times.map { random_letter }.join
+  end
+
+  def fitness
+
+    number_of_letters_in_correct_position = 0
+
+    @phrase.chars.each_index do |index|
+      number_of_letters_in_correct_position += 1 if @@TARGET_PHRASE[index] == phrase[index]
+    end
+
+    number_of_letters_in_correct_position / @@TARGET_PHRASE.length
   end
 
 private
--- spec/dna_spec.rb
+++ spec/dna_spec.rb
@@ -2,4 +2,11 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Dna do
   its('phrase.length') { should == "to be or not to be".length }
+
+  describe "#fitness" do
+    it "evaluates fitness with a score of how many letters are right" do
+      subject.stub(:phrase).and_return('to be or not xxxx')
+      subject.fitness.should == '13/18'.to_r
+    end
+  end
 end
