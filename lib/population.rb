c0dfbcc4e663a7455305a123bf506de59d8a8e74
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2013-12-07 21:44:24 +0000

    Add todos and rename

--- lib/population.rb
+++ lib/population.rb
@@ -5,15 +5,15 @@ class Population
   attr_reader :target_phrase, :mutation_rate, :population
 
   def initialize target_phrase: 'to be or not to be', mutation_rate: 0.01, population: 100
-    @target_phrase = target_phrase
-    @mutation_rate = mutation_rate
+    @target_phrase = target_phrase # TODO can't be empty
+    @mutation_rate = mutation_rate # TODO must be between 0 and 1
     @population = population # TODO population must be > 0
   end
 
   def start
-    @dnas = []
+    @current_generation = []
     # TODO should we assign mutation rate too?
-    @population.times { @dnas.push Dna.new target_phrase: target_phrase }
+    @population.times { @current_generation.push Dna.new target_phrase: target_phrase }
     @generations = 0
   end
 
@@ -25,7 +25,7 @@ class Population
 
       child = mum.crossover dad
       child.mutate
-      @dnas.push child
+      @current_generation.push child
     end
 
     @generations += 1
@@ -34,7 +34,7 @@ class Population
   def pick_parents_based_on_fitness
     mating_pool = []
 
-    @dnas.each do |dna|
+    @current_generation.each do |dna|
       n = (dna.fitness * 100).to_i # TODO bit dangerous
       n.times { mating_pool.push dna }
     end
@@ -43,10 +43,10 @@ class Population
   end
 
   def best
-    @dnas.max_by &:fitness
+    @current_generation.max_by &:fitness
   end
 
   def finished?
-    @dnas.any? { |dna| dna.genes == @target_phrase }
+    @current_generation.any? { |dna| dna.genes == @target_phrase }
   end
 end
