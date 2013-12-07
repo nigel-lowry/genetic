cbe8541e39bc44f365d95bc3275418ab29ca6131
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2013-12-07 18:09:20 +0000

    Put in keyword arguments

--- lib/population.rb
+++ lib/population.rb
@@ -1,2 +1,5 @@
 class Population
+  def initialize target_phrase: 'to be or not to be', mutation_rate: 0.01, population: 100
+
+  end
 end
--- spec/population_spec.rb
+++ spec/population_spec.rb
@@ -3,6 +3,12 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe Population do
 
   describe "#initialize" do
+    it "takes target_phrase, mutation_rate and population" do
+      @population = Population.new target_phrase: 'foobar', mutation_rate: 0.05, population: 200
+    end
 
+    it "raises error if given bad symbol" do
+      expect { Population.new foo: 'abc' }.to raise_error
+    end
   end
 end
