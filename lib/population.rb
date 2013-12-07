aea88ac772e000ac715fa3a4a1a0e8dbd978887a
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2013-12-07 18:15:21 +0000

    Assign arguments and test

--- lib/population.rb
+++ lib/population.rb
@@ -1,5 +1,9 @@
 class Population
-  def initialize target_phrase: 'to be or not to be', mutation_rate: 0.01, population: 100
+  attr_reader :target_phrase, :mutation_rate, :population
 
+  def initialize target_phrase: 'to be or not to be', mutation_rate: 0.01, population: 100
+    @target_phrase = target_phrase
+    @mutation_rate = mutation_rate
+    @population = population
   end
 end
--- spec/population_spec.rb
+++ spec/population_spec.rb
@@ -3,8 +3,12 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe Population do
 
   describe "#initialize" do
-    it "takes target_phrase, mutation_rate and population" do
-      @population = Population.new target_phrase: 'foobar', mutation_rate: 0.05, population: 200
+
+    context "setting and getting" do
+      subject { Population.new target_phrase: 'foobar', mutation_rate: 0.05, population: 200 }
+      its(:target_phrase) { should == 'foobar' }
+      its(:mutation_rate) { should == 0.05 }
+      its(:population) { should == 200 }
     end
 
     it "raises error if given bad symbol" do
